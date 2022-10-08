1d18bd58136ea43142823942305d9471978007bb
don_durito <calen36@gmail.com>
2022-10-08 22:14:03 +0300

    tweak

--- async.py
+++ async.py
@@ -1,12 +1,11 @@
 from aiohttp import web
 import ssl
-
 from main import set_webhook, get_webhook_status
 
 
 async def handle(request):
-    print(dir(request))
-    return web.Response(text='OK')
+    text = request.json
+    return web.Response(text=text)
     # return web.HTTPForbidden
 
 app = web.Application()
@@ -17,5 +16,6 @@ ssl_context.load_cert_chain('ssl/public.pem', 'ssl/private.key')
 
 
 if __name__ == '__main__':
+    set_webhook()
     get_webhook_status()
     web.run_app(app, ssl_context=ssl_context)
