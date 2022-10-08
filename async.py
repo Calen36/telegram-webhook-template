cf3479f21e17781fab592f2efb4f714577941f75
don_durito <calen36@gmail.com>
2022-10-08 23:45:29 +0300

    tweak

--- async.py
+++ async.py
@@ -1,3 +1,5 @@
+"""З"""
+
 import asyncio
 from asyncio import sleep
 
@@ -5,6 +7,7 @@ from aiogram import Bot, Dispatcher
 
 import aiohttp.web_request
 from aiohttp import web
+import aiohttp
 import ssl
 from main import set_webhook, get_webhook_status, send_message, TG_TOKEN
 
@@ -14,9 +17,7 @@ dp = Dispatcher(bot)
 
 
 
-CNT = 0
-
-async def foo(request):
+async def handle(request):
     try:
         global CNT
         r = await request.json()
@@ -35,10 +36,26 @@ async def foo(request):
         print('oops', ex)
 
 
-async def create_request_handler(request: aiohttp.web_request.Request):
-    loop.create_task(foo(request))
-    return web.Response(text='ok')
+class WebhookServer:
+    def __init__(self):
+        self.loop = asyncio.new_event_loop()
+        self.app = aiohttp.web.Application()
+        self.app.add_routes([aiohttp.web.post('/', self.create_request_handler)])
+        self.ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        self.ssl_context.load_cert_chain('ssl/public.pem', 'ssl/private.key')
 
+    def run(self):
+        aiohttp.web.run_app(self.app, ssl_context=self.ssl_context, loop=self.loop)
+
+    def create_request_handler(self, request: aiohttp.web_request.Request):
+        self.loop.create_task(handle(request))
+        return aiohttp.web.Response(text='ok')
+
+
+
+async def create_request_handler(request: aiohttp.web_request.Request):
+    loop.create_task(handle(request))
+    return web.Response(text='ok')
 
 app = web.Application()
 app.add_routes([web.post('/', create_request_handler)])
@@ -48,7 +65,11 @@ ssl_context.load_cert_chain('ssl/public.pem', 'ssl/private.key')
 
 
 if __name__ == '__main__':
-    set_webhook()
-    get_webhook_status()
-    loop = asyncio.new_event_loop()
-    web.run_app(app, ssl_context=ssl_context, loop=loop)
+    # set_webhook()
+    # get_webhook_status()
+    # loop = asyncio.new_event_loop()
+    # web.run_app(app, ssl_context=ssl_context, loop=loop)
+
+    server = WebhookServer()
+    loop = server.loop
+    server.run()
