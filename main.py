2d5f5a8098d309751e447993e9ad05084bb08118
don_durito <calen36@gmail.com>
2022-10-08 12:45:38 +0300

    tweak

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask, request
+import json
 
-app = Flask(__name__)
+with open('secr.json') as file:
+    secret = json.load(file)
+TG_TOKEN = secret['TG_TEST_TOKEN']
+
+app = Flask('Webhooks Receiver')
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -10,5 +16,40 @@ def index():
     return '<h1>ABRA! CADABRA!</h1>'
 
 
+from email import message
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+
+
+APP_HOST = '0.0.0.0'
+APP_PORT = 8000
+
+class SimpleGetHandler(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+
+    def _html(self, message):
+        content = (f"<html><body><h1>{message}</h1></body></html>")
+        return content.encode('utf8')
+
+    def do_GET(self):
+        self._set_headers()
+        message = 'Kurwa!'
+        self.wfile.write(self._html(message))
+
+
+def run_server(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_address = (APP_HOST, APP_PORT)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    # app.run(host='0.0.0.0', port=8080)
+    run_server(handler_class=SimpleGetHandler)
+
+
