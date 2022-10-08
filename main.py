8d89388457fc41b684df736d8f60e5260e4c52b1
don_durito <calen36@gmail.com>
2022-10-08 19:25:24 +0300

    tweak

--- main.py
+++ main.py
@@ -26,11 +26,9 @@ WEBAPP_HOST = '0.0.0.0'  # or ip
 WEBAPP_PORT = 8443
 
 
-
 with open('secr.json') as file:
     secret = json.load(file)
 TG_TOKEN = secret['TG_PROD_TOKEN']
-
 TG_API_URL = f'https://api.telegram.org/bot{TG_TOKEN}/'
 
 
@@ -82,50 +80,5 @@ def index():
 
 
 
-
-
-bot = Bot(token=TG_TOKEN)
-dp = Dispatcher(bot)
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    # Regular request
-    await bot.send_message(message.chat.id, message.text)
-    # or reply INTO webhook
-    return SendMessage(message.chat.id, message.text)
-
-
-async def on_startup(dp):
-    await bot.set_webhook(WEBHOOK_URL)
-    # insert code here to run it after start
-    pass
-
-
-async def on_shutdown(dp):
-    # logging.warning('Shutting down..')
-    # insert code here to run it before shutdown
-    # Remove webhook (not acceptable in some cases)
-    await bot.delete_webhook()
-    # Close DB connection (if used)
-    await dp.storage.close()
-    await dp.storage.wait_closed()
-
-
-
 if __name__ == '__main__':
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=True,
-        host=WEBAPP_HOST,
-        port=WEBHOOK_PORT,
-    )
-
-
-
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=8443, ssl_context=ssl_context)
+    app.run(host='0.0.0.0', port=8443, ssl_context=ssl_context)
